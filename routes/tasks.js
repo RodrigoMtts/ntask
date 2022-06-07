55e92ee2f6c6f55ea373d808578e2d24a0d55a95
rodrigo <rodrigo@MeuDebian>
2022-06-07 20:50:54 -0300

    Autenticação Implementada

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,61 +2,61 @@ module.exports = app => {
   const Tasks = app.models.tasks;
 
   app.route('/tasks')
+    .all(app.auth.authenticate())
     .get(async (req, res) => {
       try {
-        const result = await Tasks.findAll();
+        const where = { userId: req.user.id };
+        const result = await Tasks.findAll({ where });
         res.json(result);
-      }
-      catch (err) {
+      } catch (err) {
         res.status(412).json({ msg: err.message });
       }
     })
     .post(async (req, res) => {
       try {
+        req.body.userId = req.user.id;
         const result = await Tasks.create(req.body);
         res.json(result);
-      }
-      catch (err) {
+      } catch (err) {
         res.status(412).json({ msg: err.message });
       }
     });
+
   app.route('/tasks/:id')
+    .all(app.auth.authenticate())
     .get(async (req, res) => {
       try {
         const { id } = req.params;
-        const where = { id };
+        const where = { id, userId: req.user.id };
         const result = await Tasks.findOne({ where });
-
         if (result) {
           res.json(result);
         } else {
           res.sendStatus(404);
         }
-      }
-      catch (err) {
+      } catch (err) {
         res.status(412).json({ msg: err.message });
       }
     })
     .put(async (req, res) => {
       try {
         const { id } = req.params;
-        const where = { id };
+        const where = { id, userId: req.user.id };
+        req.body.userId = req.user.id;
         await Tasks.update(req.body, { where });
         res.sendStatus(204);
-      }
-      catch (err) {
+      } catch (err) {
         res.status(412).json({ msg: err.message });
       }
     })
     .delete(async (req, res) => {
       try {
         const { id } = req.params;
-        const where = { id };
+        const where = { id, userId: req.user.id };
         await Tasks.destroy({ where });
         res.sendStatus(204);
-      }
-      catch (err) {
+      } catch (err) {
         res.status(412).json({ msg: err.message });
       }
     });
-};
+}
